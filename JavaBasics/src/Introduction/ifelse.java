8e78d9d54ced8997154148fcabb2b4983d9a2cf5
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-17 15:50:04 +0800

    If Else concept

---
+++ JavaBasics/src/Introduction/ifelse.java
@@ -0,0 +1,36 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class ifelse {
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+        int N = scanner.nextInt();
+        // Below line skips the input if the input is not an integer
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(N%2 != 0) {
+        	System.out.println("Weird");
+        }
+        else
+        {
+        	if(N>2 && N <5) {
+        		System.out.println("Not Weird");
+        	}
+        	if(N>6 && N<20) {
+        		System.out.println("Weird");
+        	}
+        	else
+        	{
+        		System.out.println("Not Weird");
+        	}
+        }
+        scanner.close();
+
+	}
+
+}
