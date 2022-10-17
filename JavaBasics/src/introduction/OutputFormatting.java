c718b81db983bf125ae8d8b305239f3340b21f24
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-17 18:06:56 +0800

    Formatting output

---
+++ JavaBasics/src/introduction/OutputFormatting.java
@@ -0,0 +1,22 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s", s1);
+            System.out.printf("%03d\n", x);
+        }
+        System.out.println("================================");
+
+	}
+
+}
