a9b9bb0cf059b215b39ff31fa22e2f642f773d0c
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-17 14:48:58 +0800

    stdin and stdout using Scanner

---
+++ JavaBasics/src/Introduction/stdinout.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class stdinout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		// Task: read 3 integers from stdin and print them
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
