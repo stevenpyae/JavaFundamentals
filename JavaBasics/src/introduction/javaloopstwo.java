f4fa6c3dcff6c0918f976a1e995a316d59edbb36
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-22 17:49:06 +0800

    java loops two

---
+++ JavaBasics/src/introduction/javaloopstwo.java
@@ -0,0 +1,31 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class javaloopstwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            for(int j = 0; j < n; j++)
+            {
+            	int ans = a;
+                //System.out.println(j);
+                for(int k = j; k >= 0; k--)
+                {
+                	ans = (int) (ans + Math.pow(2, k) * b);
+                }
+                System.out.print(ans);
+            }
+        }
+        
+        in.close();
+	}
+
+}
