88629071a1d8d61d04b850ee7e46238a0441c150
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-22 12:32:20 +0800

    refactor

---
+++ JavaBasics/src/introduction/outputformatting.java
@@ -0,0 +1,23 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class outputformatting {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in)) {
+			System.out.println("================================");
+			for(int i=0;i<3;i++){
+			    String s1=sc.next();
+			    int x=sc.nextInt();
+			    //Complete this line
+			    System.out.printf("%-15s", s1);
+			    System.out.printf("%03d\n", x);
+			}
+		}
+        System.out.println("================================");
+
+	}
+
+}
