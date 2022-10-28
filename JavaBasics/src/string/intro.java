ec758a355ee4364a4aa54b4bdb0acded8d4176da
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-10-28 10:46:26 +0800

    String intro

---
+++ JavaBasics/src/string/intro.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)>0) {
+        	System.out.println("Yes");
+        }
+        else
+        {
+        	System.out.println("No");
+        }
+        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1) + " " + B.substring(0,1).toUpperCase()+B.substring(1));
+        
+        sc.close();
+	}
+
+}
