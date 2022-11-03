e2590e0c3c80343250245a447c2a4680143e2e73
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-11-03 17:54:19 +0800

    vaild username at least 8 characters with _ accepted

---
+++ JavaBasics/src/string/validusername.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.Scanner;
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "[a-zA-Z]{1}([a-zA-Z0-9_]{7,29}$)";
+}
+
+public class validusername {
+	private static final Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+
+	}
+
+}
