60a3131f5ef78897c5aff9faf6c5c3ae69abcce6
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-11-03 17:24:06 +0800

    Regex expression for IP addresses

---
+++ JavaBasics/src/string/myregexforIP.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.Scanner;
+
+public class myregexforIP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+	}
+
+}
+class MyRegex {
+	String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+	String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+	
+}
